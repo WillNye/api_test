3531cf06f2310fc8dbc7979957b235def1884869
Will <willbeas88@gmail.com>
2021-10-12 19:58:57 -0500

    Fixed mixin url

--- api_test/mixins.py
+++ api_test/mixins.py
@@ -8,19 +8,19 @@ class DeleteMixin:
         self.delete()
 
     def test_invalid_id(self):
-        response = self.session.delete(f'{self.url}/{self.route}/a01')
+        response = self.session.delete(f'{self.base_url}/{self.route}/a01')
         self.assertEqual(response.status_code, 404)
 
         return response
 
     def test_id_does_not_exist(self):
-        response = self.session.delete(f'{self.url}/{self.route}/{uuid4()}')
+        response = self.session.delete(f'{self.base_url}/{self.route}/{uuid4()}')
         self.assertEqual(response.status_code, 404)
 
         return response
 
     def test_unauth(self):
-        response = requests.delete(f'{self.url}/{self.route}/{self.object_id}')
+        response = requests.delete(f'{self.base_url}/{self.route}/{self.object_id}')
         self.assertEqual(response.status_code, 401)
 
         return response
@@ -30,7 +30,7 @@ class ListMixin:
 
     def test_filter(self, filter_key: str):
         filter_val = self.object[filter_key]
-        response = self.session.get(f'{self.url}/{self.route}', params={filter_key: filter_val})
+        response = self.session.get(f'{self.base_url}/{self.route}', params={filter_key: filter_val})
         self.assertTrue(response.ok)
         response_objs = response.json()
         self.assertTrue(all(obj[filter_key] == filter_val for obj in response_objs['results']))
@@ -38,7 +38,7 @@ class ListMixin:
         return response
 
     def test_sort(self, sort_param: str, sort_key: str):
-        response = self.session.get(f'{self.url}/{self.route}', params={sort_param: sort_key})
+        response = self.session.get(f'{self.base_url}/{self.route}', params={sort_param: sort_key})
         self.assertTrue(response.ok)
 
         if sort_key.startswith('-'):
@@ -57,6 +57,6 @@ class ListMixin:
 class RetrieveMixin:
 
     def test_valid_retrieve(self):
-        response = self.session.get(f'{self.url}/{self.route}/{self.object_id}')
+        response = self.session.get(f'{self.base_url}/{self.route}/{self.object_id}')
         self.assertTrue(response.ok)
 
